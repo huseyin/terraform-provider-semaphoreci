69195a7fdef2a655d51175d0d0e5262582063e78
Hüseyin Tekinaslan <htkaslan@gmail.com>
2019-10-08 10:53:00 +0300

    .

--- semaphoreci/data_source_projects.go
+++ semaphoreci/data_source_projects.go
@@ -1,16 +1,37 @@
 package semaphoreci
 
-import "github.com/hashicorp/terraform/helper/schema"
+import (
+	"fmt"
+	"log"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/semaphoreci/cli/api/client"
+)
 
 // dataSourceSemaphoreCIProjects returns a *schema.Resource.
 func dataSourceSemaphoreCIProjects() *schema.Resource {
 	return &schema.Resource{
-		Read:   dataSourceSemaphoreCIProjectsRead,
-		Schema: map[string]*schema.Schema{},
+		Read: dataSourceSemaphoreCIProjectsRead,
+
+		Schema: map[string]*schema.Schema{
+			"projects": {
+				Type:     schema.TypeList,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Computed: true,
+			},
+		},
 	}
 }
 
 // TODO
 func dataSourceSemaphoreCIProjectsRead(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG] Reading projects")
+
+	projectAPI := client.NewProjectV1AlphaApi()
+	_, err := projectAPI.ListProjects()
+	if err != nil {
+		return fmt.Errorf("error reading projects from SemaphoreCI: %s", err)
+	}
+
 	return nil
 }
